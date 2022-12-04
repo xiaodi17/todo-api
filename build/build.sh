f6782ec4aa38db9c77f3e6ca23e2f2f991342663
David Lu <david.lu@ofx.com>
2022-12-04 15:24:45 +1100

    actions

---
+++ build/build.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+cleanup() {
+  echo "Cleaning up..."
+  docker-compose \
+    -p $project \
+    -f ../docker-compose.tests.yml \
+    -f ../docker-compose.yml down \
+    --remove-orphans || true
+
+  docker stop $(docker ps -a -q) || true
+  docker rm $(docker ps -a -q) || true
+  docker image rm "${project}:${GIT_SHA}" || true
+}
+
+: ${GIT_SHA?"GIT_SHA env variable is required"}
+: ${VERSION?"VERSION env variable is required"}
+project="counterparty-instructions-api"
+
+cleanup
+
+echo "Running the tests..."
+# docker-compose -f ../docker-compose.yml -f ../docker-compose.tests.yml pull
+# docker-compose \
+#   -p $project \
+#   -f ../docker-compose.yml \
+#   -f ../docker-compose.tests.yml \
+#   up \
+#   --build --force-recreate --remove-orphans \
+#   --exit-code-from tests \
+#   --abort-on-container-exit \
+#   tests
+
+cleanup
