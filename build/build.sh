0d0894fb90cbb21d2682d50531c0c2def1427bcc
David Lu <david.lu@ofx.com>
2022-12-14 01:57:05 +1100

    add test in gh

--- build/build.sh
+++ build/build.sh
@@ -21,15 +21,15 @@ project="counterparty-instructions-api"
 cleanup
 
 echo "Running the tests..."
-# docker-compose -f ../docker-compose.yml -f ../docker-compose.tests.yml pull
-# docker-compose \
-#   -p $project \
-#   -f ../docker-compose.yml \
-#   -f ../docker-compose.tests.yml \
-#   up \
-#   --build --force-recreate --remove-orphans \
-#   --exit-code-from tests \
-#   --abort-on-container-exit \
-#   tests
+docker-compose -f ../docker-compose.yml -f ../docker-compose.tests.yml pull
+docker-compose \
+  -p $project \
+  -f ../docker-compose.yml \
+  -f ../docker-compose.tests.yml \
+  up \
+  --build --force-recreate --remove-orphans \
+  --exit-code-from tests \
+  --abort-on-container-exit \
+  tests
 
 cleanup
---
+++ test.dockerfile
@@ -0,0 +1,11 @@
+FROM mcr.microsoft.com/dotnet/sdk:6.0
+WORKDIR /app/
+COPY ./src/Todo.Api/*.csproj ./src/Todo.Api/
+COPY ./src/Todo.Tests/*.csproj ./src/Todo.Tests/
+COPY ./todo-api.sln ./
+
+RUN dotnet restore
+
+COPY ./src/ ./src/
+
+ENTRYPOINT ["dotnet", "test"]
